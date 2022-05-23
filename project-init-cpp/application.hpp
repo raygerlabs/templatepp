f8de1396e8089e21327c1291c38f3dd2995526e8
Rajmund Kail <105101363+raygerlabs@users.noreply.github.com>
2022-05-23 12:41:12 +0200

    [Feature] Add build system (#1)
    * cmake-conan: add simple build system
    
    * cmake-conan: add cross-platform build workflow
    
    * cmake-conan: add CMAKE_MODULE_PATH in CMakeLists.txt
    
    * cmake-conan: force at least c++17, fpic and elf visibility hidden
    
    * cmake-conan: add license file
    
    * cmake-conan: add release workflow
    
    * cmake-conan: update readme.md

---
+++ project-init-cpp/application.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <details/configure.hpp>
+
+namespace rglabs
+{
+class RGLABS_LOCAL application
+{
+public:
+  void RGLABS_API start();
+  void RGLABS_API stop();
+};
+} // namespace rglabs
