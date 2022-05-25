064c508e96e615223bb85dd9ab2f2b3a6656ed61
raygerlabs <raygerlabs@gmail.com>
2022-05-25 11:43:54 +0200

    Rename test to tests

--- test/application_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <application.hpp>
-#include <gtest/gtest.h>
-
-TEST(application_test, application_starts_and_stops_gracefully)
-{
-  rglabs::application app{};
-  ASSERT_NO_THROW(app.start());
-  ASSERT_NO_THROW(app.stop());
-}
