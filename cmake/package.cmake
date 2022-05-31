8d7f3af1ecbaff1dd022c5674bfb4040af47e657
Rajmund Kail <105101363+raygerlabs@users.noreply.github.com>
2022-05-31 19:17:41 +0200

    Add cpack support (#20)
    * Add cpack support
    
    * Remove unnecessary files
    
    * Save archive extension
    
    * Duplicated extension?
    
    * Remove cmake archive file variable as it is used something else for
    
    * ...
    
    * Add logs
    
    * Revert package archival in conanfile.py
    
    * Fix paths
    
    * Use cpack instead of custom function in conan
    
    * Append package file name with architecture
    
    * Remove caching
    
    * Architecture?
    
    * Path
    
    * Force package name to be lower case
    
    * Cache package information
    
    * Remove custom function from conanfile.py in favor of cpack
    
    * Swap package overrides before include(CPack)
    
    * Re-add cmake cache
    
    * Bump version
    
    * Fix package generation

---
+++ cmake/package.cmake
@@ -0,0 +1,44 @@
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+# Package generators
+# Generate tar.gz packages by default
+set(CPACK_GENERATOR "TGZ")
+set(CPACK_SOURCE_GENERATOR "TGZ")
+set(CPACK_PACKAGE_FILE_EXTENSION ".tar.gz")
+if (WIN32)
+  # Otherwise for Windows generate zip files
+  set(CPACK_GENERATOR "ZIP")
+  set(CPACK_SOURCE_GENERATOR "ZIP")
+  set(CPACK_PACKAGE_FILE_EXTENSION ".zip")
+endif()
+
+# Package information
+set(CPACK_PACKAGE_NAME "${PROJECT_NAME}")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "${PROJECT_DESCRIPTION}")
+set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
+
+# Package documentation
+set(CPACK_RESOURCE_FILE_README "${PROJECT_SOURCE_DIR}/README.md")
+set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE")
+
+# Add Cpack support
+include(CPack)
+
+# NOTE: Some of these values set by CPack, therefore must be AFTER the include!
+# Default package overrides
+set (CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_SYSTEM_NAME}")
+if (CPACK_PACKAGE_ARCHITECTURE)
+  set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_FILE_NAME}-${CPACK_PACKAGE_ARCHITECTURE}")
+endif()
+string(TOLOWER "${CPACK_PACKAGE_FILE_NAME}" CPACK_PACKAGE_FILE_NAME)
+set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_FILE_NAME}" CACHE STRING "CPack package file name")
+
+# Generate package information
+set(CPACK_FULL_PACKAGE_FILE_NAME "${CPACK_PACKAGE_FILE_NAME}${CPACK_PACKAGE_FILE_EXTENSION}")
+string(TOLOWER "${CPACK_FULL_PACKAGE_FILE_NAME}" CPACK_FULL_PACKAGE_FILE_NAME)
+file(WRITE "${PROJECT_BINARY_DIR}/package_info.txt" "${CPACK_FULL_PACKAGE_FILE_NAME}")
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import shutil
 
 class templatepp(ConanFile):
   name = "templatepp"
-  version = "0.2"
+  version = "0.3.0"
   license = "MIT"
   author = "Rajmund Kail"
   url = "https://www.github.com/raygerlabs/templatepp"
@@ -46,23 +46,10 @@ class templatepp(ConanFile):
     cmake.build()
     cmake.test()
 
-  def make_artifact(self):
-    artifact_name = f"{self.name}-v{self.version}-{self.settings.os}-{self.settings.arch}"
-    artifact_name = artifact_name.lower()
-    artifact_format = "gztar"
-    if self.settings.os == "Windows":
-      artifact_format = "zip"
-    artifact_path = shutil.make_archive(artifact_name, artifact_format, self.package_folder)
-    with open("artifact_name.txt", "w") as fartifact_name:
-      print(f"{os.path.basename(artifact_path)}", file=fartifact_name)
-    with open("artifact_path.txt", "w") as fartifact_path:
-      print(f"{artifact_path}", file=fartifact_path)
-    self.output.success(f"Artifact '{os.path.basename(artifact_path)}' created")
-
   def package(self):
     cmake = self.configure_cmake()
     cmake.install()
-    self.make_artifact()
+    self.run("cpack", self.build_folder)
 
   def package_info(self):
     self.cpp_info.libs = tools.collect_libs(self)
