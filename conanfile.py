84b252a6d02cd50ce69dc8375ecb6b441077037d
raygerlabs <raygerlabs@gmail.com>
2022-06-04 11:24:09 +0200

    Add user options in conan

--- conanfile.py
+++ conanfile.py
@@ -10,8 +10,8 @@ class TemplateppRecipe(ConanFile):
   url = "https://www.github.com/raygerlabs/templatepp"
   description = "A cross-platform project template for modern C and C++"
   settings = "os", "compiler", "build_type", "arch"
-  options = { "shared": [True, False] }
-  default_options = { "shared": True }
+  options = { "shared": [True, False], "with_tests": [True, False], "with_presets": [True, False], "with_packaging": [True, False] }
+  default_options = { "shared": True, "with_tests": False, "with_presets": False, "with_packaging": True }
   generators = "CMakeDeps"
   exports = ("LICENSE")
   exports_sources = ("cmake/*",
@@ -37,14 +37,19 @@ class TemplateppRecipe(ConanFile):
 
   def configure_cmake(self):
     cmake = CMake(self)
-    if self.settings.compiler == "Visual Studio":
-      cmake.configure(args=["--preset=msvc"])
-    elif self.settings.compiler == "clang":
-      cmake.configure(args=["--preset=clang"])
-    elif self.settings.compiler == "apple-clang":
-      cmake.configure(args=["--preset=clang"])
-    elif self.settings.compiler == "gcc":
-      cmake.configure(args=["--preset=gcc"])
+    cmake.definitions["BUILD_TESTING"] = self.options.with_tests
+    cmake.definitions["BUILD_PACKAGING"] = self.options.with_packaging
+    if self.options.with_presets:
+      if self.settings.compiler == "Visual Studio":
+        cmake.configure(args=["--preset=msvc"])
+      elif self.settings.compiler == "clang":
+        cmake.configure(args=["--preset=clang"])
+      elif self.settings.compiler == "apple-clang":
+        cmake.configure(args=["--preset=clang"])
+      elif self.settings.compiler == "gcc":
+        cmake.configure(args=["--preset=gcc"])
+      else:
+        cmake.configure()
     else:
       cmake.configure()
     cmake.verbose = True
@@ -53,12 +58,14 @@ class TemplateppRecipe(ConanFile):
   def build(self):
     cmake = self.configure_cmake()
     cmake.build()
-    cmake.test()
+    if self.options.with_tests:
+      cmake.test()
 
   def package(self):
     cmake = self.configure_cmake()
     cmake.install()
-    self.run("cpack", self.build_folder)
+    if self.options.with_packaging:
+      self.run("cpack", self.build_folder)
 
   def package_info(self):
     self.cpp_info.libs = tools.collect_libs(self)
