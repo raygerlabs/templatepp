a5cb176a8566bc7cb3f4920609ce3d981744d3f7
raygerlabs <raygerlabs@gmail.com>
2022-06-08 10:46:13 +0200

    Add install rpath

--- cmake/install_paths.cmake
+++ cmake/install_paths.cmake
@@ -2,6 +2,10 @@
 #------------------------------------------------------------------------------
 string(TOUPPER ${PROJECT_NAME} PACKAGE_PREFIX)
 #------------------------------------------------------------------------------
+if(NOT APPLE)
+    set(CMAKE_INSTALL_RPATH $ORIGIN)
+endif()
+#------------------------------------------------------------------------------
 set(PACKAGE_INSTALL_BINDIR bin)
 set(PACKAGE_INSTALL_LIBDIR lib)
 set(PACKAGE_INSTALL_INCLUDEDIR include)
